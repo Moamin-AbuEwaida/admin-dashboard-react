569533a90bddbbdef72dcced8dc1ee8526b2cc69
Moamin <moamin.salamah@gmail.com>
2022-10-09 12:41:35 +0200

    users list page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import './app.css'
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
 import Home from './pages/home/Home';
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import UserList from './pages/userList/UserList';
 
 function App() {
   return (
-    <div>
+    <Router>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route exact path='' element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/dummyData.js
+++ src/dummyData.js
@@ -44,3 +44,96 @@ export const userData = [
     "Active User": 1000,
   },
 ];
+
+export const userRows = [
+  {
+    id: 1,
+    username: "June Maril",
+    Avatar:
+      "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg?cs=srgb&dl=pexels-mateus-souza-3586798.jpg&fm=jpg",
+    email: "june@gmail.com",
+    status: "active",
+    transaction: "€124",
+  },
+  {
+    id: 2,
+    username: "Jon Snow",
+    Avatar:
+      "https://images.pexels.com/photos/1170261/pexels-photo-1170261.jpeg?cs=srgb&dl=pexels-alexander-krivitskiy-1170261.jpg&fm=jpg",
+    email: "jon@gmail.com",
+    status: "active",
+    transaction: "€543",
+  },
+  {
+    id: 3,
+    username: "Maricel Jack",
+    Avatar:
+      "https://images.pexels.com/photos/2834009/pexels-photo-2834009.jpeg?cs=srgb&dl=pexels-trace-constant-2834009.jpg&fm=jpg",
+    email: "Majack@gmail.com",
+    status: "active",
+    transaction: "€123",
+  },
+  {
+    id: 4,
+    username: "Cecil katra",
+    Avatar:
+      "https://images.pexels.com/photos/9167549/pexels-photo-9167549.jpeg?cs=srgb&dl=pexels-dmitriy-ganin-9167549.jpg&fm=jpg",
+    email: "csci@gmail.com",
+    status: "active",
+    transaction: "€146",
+  },
+  {
+    id: 5,
+    username: "Jo larak",
+    Avatar:
+      "https://images.pexels.com/photos/9254560/pexels-photo-9254560.jpeg?cs=srgb&dl=pexels-ron-lach-9254560.jpg&fm=jpg",
+    email: "jo@gmail.com",
+    status: "active",
+    transaction: "€34",
+  },
+  {
+    id: 6,
+    username: "Jerar Dorr",
+    Avatar:
+      "https://images.pexels.com/photos/13717469/pexels-photo-13717469.jpeg?cs=srgb&dl=pexels-dominika-mazur-13717469.jpg&fm=jpg",
+    email: "jerar@gmail.com",
+    status: "active",
+    transaction: "€234",
+  },
+  {
+    id: 7,
+    username: "Ali Rami",
+    Avatar:
+      "https://images.pexels.com/photos/5474028/pexels-photo-5474028.jpeg?cs=srgb&dl=pexels-cottonbro-5474028.jpg&fm=jpg",
+    email: "ARami@gmail.com",
+    status: "active",
+    transaction: "€1245",
+  },
+  {
+    id: 8,
+    username: "Charel Noam",
+    Avatar:
+      "https://images.pexels.com/photos/9818341/pexels-photo-9818341.jpeg?cs=srgb&dl=pexels-valera-evane-9818341.jpg&fm=jpg",
+    email: "Cha@gmail.com",
+    status: "active",
+    transaction: "€374",
+  },
+  {
+    id: 9,
+    username: "Mami Akon",
+    Avatar:
+      "https://images.pexels.com/photos/11440539/pexels-photo-11440539.jpeg?cs=srgb&dl=pexels-ottun-abdulmalik-11440539.jpg&fm=jpg",
+    email: "mami@gmail.com",
+    status: "active",
+    transaction: "€34",
+  },
+  {
+    id: 10,
+    username: "Noal Karoame",
+    Avatar:
+      "https://images.pexels.com/photos/10678506/pexels-photo-10678506.jpeg?cs=srgb&dl=pexels-cottonbro-10678506.jpg&fm=jpg",
+    email: "nona@gmail.com",
+    status: "active",
+    transaction: "€843",
+  },
+];
